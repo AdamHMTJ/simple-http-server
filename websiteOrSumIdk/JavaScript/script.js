3f525ac992af76dc833eebb361aa6120cd802cce
Adam Helo <adammroueh20@gmail.com>
2026-09-21 13:37:21 -0400

    working is text read

--- websiteOrSumIdk/JavaScript/script.js
+++ websiteOrSumIdk/JavaScript/script.js
@@ -1,14 +1,16 @@
-var groceries = ['milk', 'Eggs', 'Cheese']
+const text = document.getElementById('text')
 
+const str = 'this is the test to see if this whole thing works, because I need to know'
+const strArray = str.split(' ')
 
-
-function listGroceries(){
-    for (i = 0; i< groceries.length ; i++){
-    console.log(groceries.at(i))
+function timeout(ms){
+ return new Promise(resolve => setTimeout(resolve, ms))
 }
-}
-listGroceries()
 
-document.getElementById('box').addEventListener('click', function(){
-    alert('i got clicked')
-})
+async function Read() {
+    for(i = 0; i < strArray.length; i++){
+        text.textContent = strArray[i]
+        await timeout(100)
+        console.log(i)
+    }
+}
